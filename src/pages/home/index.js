f9f3d91813d7d5548cb596ca3d170792daed8a98
ACVBdeveloper <124077498+ACVBdeveloper@users.noreply.github.com>
2023-03-02 19:10:03 -0300

    atualização da filmaria

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,17 +1,19 @@
 import {useEffect, useState} from 'react';
 import api from '../../services/api';
+import{Link} from 'react-router-dom';
+import './home.css';
 
 // URL :/movie/550?api_key=c07e5f599e8c80173e821b413f7e1dc6
 
 
 function Home(){
 
-    const [fil,es, setFilmes] = useState([])
+    const [filmes, setFilmes] = useState([]);
 
     useEffect(()=> {
 
         async function loadFilmes(){
-            const response = await api.get("movie", {
+            const response = await api.get("movie/now_playing", {
                 params:{
                     api_key:"c07e5f599e8c80173e821b413f7e1dc6",
                     language: "pt-BR",
@@ -19,7 +21,8 @@ function Home(){
                 }
             })
        
-            console.log(response)
+            //console.log(response.data.results.slice(0, 10));
+            setFilmes(response.data.results.slice(0, 10))  
         }
 
         loadFilmes();
@@ -27,7 +30,19 @@ function Home(){
     }, [])
 
     return(
-        <div>
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme) => {
+
+                    return(
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={'https://image.tmdb.org/t/p/original/${filme.backdrop_path}'} alt={filme.title} />
+                            <Link to={'/filme/${filme.id}'} >Acessar </Link>
+                        </article>
+                    )
+                })}    
+            </div>
             <h1>Bem vindo a Home</h1>
         </div>
     )
